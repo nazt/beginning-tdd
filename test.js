6e12ed44ff6f6ec60afe1acd46191ca9d5b42e5a
Nat <nat.weerawan@gmail.com>
2011-04-04 17:50:05 +0700

    Add setUp and tearDown comment

--- test.js
+++ test.js
@@ -14,11 +14,24 @@ YUI().use('test', 'console', function (Y) {
 
     var paddle = new Y.Test.Case({
         name: 'paddle',
+        //---------------------------------------------------------------------
+        // setUp and tearDown methods - optional
+        //---------------------------------------------------------------------
 
+        /*
+         * Sets up data that is needed by each test.
+         */
         setUp: function () {
             this.paddle = Object.create(PONG.paddle1);
             this.paddle.height = 100;
         },
+        /*
+         * Cleans up everything that was created by setUp().
+         */
+        tearDown : function () {
+            delete this.data;
+        },
+
       });
 
       Y.Test.Runner.add(paddle);
