1af8f8b71616f84cb84da31ff0754df89d0ac90f
Nat <nat.weerawan@gmail.com>
2011-04-28 14:17:47 +0700

    addOption to choose console type

--- test.js
+++ test.js
@@ -32,24 +32,23 @@ YUI().use('test', 'console', function (Y) {
           testName: function () {
               Y.Assert.areEqual("Nicholas", this.data.name, "Name should be 'Nicholas'");
           },
-
           testAge: function () {
               Y.Assert.areEqual(28, this.data.age, "Age should be 28");
           }
-
       });
-
       //example test suite
       var ExampleSuite = new Y.Test.Suite("Example Suite");
       ExampleSuite.add(testCase);
 
-      //create the console
-      var r = new Y.Console({
-          newestOnTop : true,
-          style: 'block' // to anchor in the example content
-      });
-
-      r.render('#testLogger');
+      //Use console on Web or Use console.log
+      var useWebConsole = false;
+        if (useWebConsole) {
+        var r = new Y.Console({
+            newestOnTop : true,
+            style: 'block' // to anchor in the example content
+        });
+        r.render('#testLogger');
+      } //ConsoleType
 
       //run test
       Y.Test.Runner.add(ExampleSuite);
